f384fab31083f7a0ef8b99a563f57750aad3b314
junseokho <hgsrun@gmail.com>
2023-12-06 14:49:17 +0900

    책 검색 기능 API 구현(#3)

---
+++ sbb/src/main/java/com/mysite/sbb/DTO/BookSearchResponseDTO.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class BookSearchResponseDTO {
+    private String title;
+    private String link;
+    private String image;
+    private String author;
+    private String discount;
+    private String publisher;
+    private String pubdate;
+    private String isbn;
+    private String description;
+}
